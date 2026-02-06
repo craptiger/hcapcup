3b7169add2509a3738dfe57c35944719d2ed3baf
craptiger <craptiger@hotmail.com>
2026-02-06 17:59:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
    - Running totals displayed per row (handicap base + cumulative points)
    - Persists to localStorage
 */
-const APP_VERSION = "1.0.4";
+const APP_VERSION = "1.0.5";
 
 const STORAGE_KEY = `handicap-cup-${APP_VERSION}`;
 
