c6630c8b206b8f58b174969cf5e0d5283d836b25
craptiger <craptiger@hotmail.com>
2026-02-06 16:05:01 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,11 @@ const ASSETS = [
   "./app.js",
   "./manifest.webmanifest"
 ];
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
